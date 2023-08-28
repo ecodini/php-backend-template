4071e1727807d6b3840be437133ad59a6b8237c3
ecodini <ecodini1234@gmail.com>
2023-08-28 13:54:42 -0300

    feat: add delete all

--- src/Lib/Db/DbService.php
+++ src/Lib/Db/DbService.php
@@ -1,5 +1,7 @@
 <?php namespace Holamanola45\Www\Lib\Db;
 
+use Exception;
+
 abstract class DbService {
     private DbConnection $db_conn;
 
@@ -66,6 +68,26 @@ abstract class DbService {
             DELETE FROM ' . $this->table_name . '
             WHERE id = :id;
         ', array('id' => $id));
+
+        if (count($rows) == 0) {
+            return NULL;
+        }
+
+        return $rows[0];
+    }
+
+    public function deleteAll(array $query_params, bool $force = false) {
+        $where = QueryGenerator::generateWhere($query_params);
+
+        $rows = NULL;
+
+        if (isset($where)) {
+            $rows = $this->query('DELETE FROM ' . $this->table_name . ' ' . $where, $query_params["where"]);
+        } else if (!$force) {
+            throw new Exception('Delete from without where detected! Use force flag to force delete. WARNING: This will delete ALL rows!');
+        } else {
+            $rows = $this->query('DELETE FROM ' . $this->table_name);
+        }
 
         if (count($rows) == 0) {
             return NULL;
